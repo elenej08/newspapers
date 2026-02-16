0ad6ba47daf7ef2f75da1abab83c7d015d18fd01
elenej08 <jugelielene09@gmail.com>
2026-02-16 22:27:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 from exstension import app
-from flask import Flask, render_template
-
-app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/box1')
-def box1():
-    return render_template('box1.html')
-
-@app.route('/box2')
-def box2():
-    return render_template('box2.html')
-
-@app.route('/box3')
-def box3():
-    return render_template('box3.html')
 
 if __name__ == '__main__':
+    import routes1
     app.run(debug=True)
+
 
 
 
