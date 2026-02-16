effe03f7958cfc0dea128975b363eab19479ba0c
elenej08 <jugelielene09@gmail.com>
2026-02-16 22:36:37 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
+from flask import render_template
 from exstension import app
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/box1')
+def  box1():
+    return render_template('box1.html')
 
-if __name__ == '__main__':
-    import routes1
-    app.run(debug=True)
+@app.route('/box2')
+def box2():
+    return render_template('box2.html')
+
+@app.route('/box3')
+def box3():
+    return render_template('box3.html')
+
 
 
 
