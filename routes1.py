7657b84589fee2a6b34dc5a4bed852da63c82892
elenej08 <jugelielene09@gmail.com>
2026-02-16 21:53:05 +0400

    Add files via upload

---
+++ routes1.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from exstension import app
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/box1')
+def  box1():
+    return render_template('box1.html')
+
+@app.route('/box2')
+def box2():
+    return render_template('box2.html')
+
+@app.route('/box3')
+def box3():
+    return render_template('box3.html')
+
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from routes1 import *
+from exstension import app
+
+app.run(host='0.0.0.0', debug=True)
